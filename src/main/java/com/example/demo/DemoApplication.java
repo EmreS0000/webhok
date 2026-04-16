c360fdd29ca2fdb62db6538efc9c9f276780e4cc
EmreS0000 <149138449+EmreS0000@users.noreply.github.com>
2026-04-16 10:43:19 +0300

    son

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-
+//abcd
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@GetMapping("/")
 	public String hello() {
-		return "Merhaba! Jenkins, Docker ve GitHub otomatik CI/CD akisi basariyla calisiyor!";
+		return "🌟 Muazzam! GitHub Webhook tetiklendi, kod Jenkins'e ulasti ve Docker tarafindan basariyla guncellendi! (Yeni Versiyon v2.0)";
 	}
 }
